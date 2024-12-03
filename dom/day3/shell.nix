f2e0a66f08727877b32a8c97bd96cfa2e1806c17
Dominic Mason <dominicmason555@gmail.com>
2024-12-03 07:10:01 +0000

    Dom day 3

--- dom/day3/shell.nix
+++ dom/day3/shell.nix
@@ -2,7 +2,7 @@ let
   pkgs = import <nixpkgs> {};
 in
 pkgs.mkShell {
-    name = "day2";
+    name = "day3";
     nativeBuildInputs = with pkgs.buildPackages; [
         just
         lazygit
@@ -13,7 +13,7 @@ pkgs.mkShell {
     shellHook = ''
         echo ""
         echo "Advent of Code 2024"
-        echo "Day 2: Red-Nosed Reports - https://adventofcode.com/2024/day/2"
+        echo "Day 3: Mull It Over - https://adventofcode.com/2024/day/3"
         echo "Hy docs: https://hylang.org/hy/doc/v1.0.0"
         echo ""
     '';
