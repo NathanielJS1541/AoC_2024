2161fb70b3fbbf09ba07be036d593b8b589f33e9
David Way <david@david-way.uk>
2024-12-07 12:47:06 +0000

    Day 3 pt 2

--- david/3_Mull_it_over/src/main.rs
+++ david/3_Mull_it_over/src/main.rs
@@ -1,4 +1,5 @@
-use std::{fs::{self, File}, io::{self, Read}};
+use std::fs;
+use std::io;
 
 // it's regex time
 use regex::Regex;
@@ -8,22 +9,39 @@ fn main() -> Result<(), io::Error> {
     // Read the file
     let input: String = fs::read_to_string(path)?;
 
+    // Part 1
+    let operands = find_pairs_in_string(&input);
+    // Multiply and sum results
+    let total: i32 = operands.iter().map(|s: &(i32, i32)| s.0 * s.1).sum();
+
+    println!("Part 1 Total: {}", total);
+
+    // Part 2
+
+    // This expression selects everything we want to remove from the input
+    let expression = Regex::new(r"(don't\(\))[\s\S]*?((do\(\))|$)").unwrap();
+    let filtered: String = expression.replace_all(&input, "").to_string();
+    
+    let operands_2 = find_pairs_in_string(&filtered);
+    // Multiply and sum results
+    let total_2: i32 = operands_2.iter().map(|s: &(i32, i32)| s.0 * s.1).sum();
+
+    println!("Part 2 Total: {}", total_2);
+
+
+    // a-ok :)
+    return Ok(());
+}
+
+fn find_pairs_in_string(haystack: &String) -> Vec<(i32, i32)> {
     // named capturing groups a,b refer to the operands
     let expression = Regex::new(r"mul\((?<a>\d+),(?<b>\d+)\)").unwrap();
 
     // Loop over captures and store pairs of operands
-    let mut operands: Vec<(i32, i32)> = vec![];
-    for captures in expression.captures_iter(&input) {
-        operands.push( ((captures["a"].parse::<i32>().unwrap()), (captures["b"].parse::<i32>().unwrap())) );
+    let mut results: Vec<(i32, i32)> = vec![];
+    for captures in expression.captures_iter(&haystack) {
+        results.push( ((captures["a"].parse::<i32>().unwrap()), (captures["b"].parse::<i32>().unwrap())) );
     }
 
-    // println!("Operands: {:?}", operands);
-
-    // Multiply and sum results
-    let total: i32 = operands.iter().map(|s: &(i32, i32)| s.0 * s.1).sum();
-
-    println!("Total: {}", total);
-
-    // a-ok :)
-    return Ok(());
+    return results;
 }
